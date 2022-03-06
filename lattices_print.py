631d3388234df464ea863795c5e2ccade75da5ca
Pico <lori.pichi@gmail.com>
2022-03-06 20:29:45 +0100

    documentation of LatexGraph and LatexVertex

--- lattices_print.py
+++ lattices_print.py
@@ -5,6 +5,9 @@ import ast
 import time
 
 def print_preambles():
+    '''
+    This function prints the needed TIKZ-preambles for the LaTex document.
+    '''
     print("""% ========== Tikz setting ==========
 %\\usepackage[svgnames]{xcolor}
 \\usepackage{tikz}
@@ -43,6 +46,40 @@ def print_preambles():
 """)
     
 class LatexGraph:
+    """
+    Given a graph's G, a LatexGraph is a Python data-structur that contains all the G's necessary information to
+    to prints it as a .tikz and .tex file.
+    It is composed by 9 parameters:
+        -----------
+        > vertices : set
+            Each vertex of G can be represented with a LatexVertex (see its documentation); this set contains
+            a LatexVertex for each vertex of G.
+        
+        > numVertices : int
+            It saves the number of vertices which compose G
+                
+        > output : FILE
+            It is the file in which printing the output LaTex code (if fp is None, the code is printed on the terminal)
+            
+        > node_style  : str
+            It represents the node's style to use for printing the vertices of G. The node_style must be one between 'rn',
+                'gn', 'yn', 'blstyle', 'wstyle', 'little', 'none', and the other one defined in the preambles.
+                
+        > edges_style  : str
+            It represents the edge's style to use for printing the edges of G. The edges_style must be one between 'simple',
+                'arrow', 'tick', 'redstyle', 'bluestyle', 'greenstyle', 'redarrow', and the other one defined in the preambles.
+                
+        > prefix : str
+        > sufix  : str
+        > title  : str
+            These three field are used to print some text before and after the tikz code
+            
+        > nodeprefix : str
+            This string can be usefull for generate overlapped graphs without generate not real edges
+        
+        -----------
+    """
+    
     def __init__(self, fp= None):
         self.vertices = {}
         self.numVertices = 0
@@ -56,12 +93,14 @@ class LatexGraph:
 
     # ---------------- Graph function ---------------------        
     def addVertex(self,key,position,name=None,color=None):
+        """ It adds a vertex to the graph """
         self.numVertices = self.numVertices + 1
         newVertex = LatexVertex(key,position,name,color)
         self.vertices[key] = newVertex
         return newVertex
     
     def getVertex(self,n):
+        """ It returns the vertex with id n """
         if n in self.vertices:
             return self.vertices[n]
         else:
@@ -71,16 +110,19 @@ class LatexGraph:
         return n in self.vertices
     
     def addEdge(self,f,t,cost=0):
-            self.vertices[f].addNeighbor(self.vertices[t],cost)
-            #self.vertices[t].addNeighbor(self.vertices[f],cost)
+        """ It adds an edge to the graph """
+        self.vertices[f].addNeighbor(self.vertices[t],cost)
     
     def getVertices(self):
+        """ It returns the vertices of G """
         return list(self.vertices.keys())
         
     def __iter__(self):
         return iter(self.vertices.values())
     
-    # ------------- Printing function ---------------------
+    # --------------------------- Printing function -----------------------------------
+    # The following functions are used to prepare the parameters for the Tikz functions
+    
     def set_node_style(self, string):
         self.node_style = string
         
@@ -110,15 +152,19 @@ class LatexGraph:
         self.title = string
         
     def article(self):
+        """ This function is used to print the graph in a LaTex file of class article """
         self.set_prefix("\subsubsection{%s}")
         self.set_sufix("")
         
     def beamer(self, supertitle= ""):
+        """ This function is used to print the graph in a Beamer file """
         s = "\\begin{frame}%s" % (supertitle)
         self.set_prefix(s + "{%s}")
         self.set_sufix("\end{frame}")
         
-    # ----------------- Tikz function ---------------------
+    # --------------------- Tikz functions -------------------------
+    # The following functions are used to print the LaTex/Tikz code
+    
     def nodes(self):
         for i in list(self.vertices.keys()):
             v = self.getVertex(i)
@@ -171,6 +217,28 @@ class LatexGraph:
         print(self.sufix, file= self.output)
                 
 class LatexVertex:
+    """
+    A LatexVertex is composed by 5 parameters:
+        -----------
+        > id : int
+            It contains a number to univocally identify this particular vertex.
+        
+        > connectedTo : dictionary ({key1: value1, key2: valu2, ...})
+            Every element is composed by oneother LatexVertex as kay and a weight (of arbitrary type) as value.
+            es. if the element v.connectedTo = {w: 10, z: 3} then the vertex v is connected to the vertex w with
+                weight 10 and to the vertex z with weight 3.
+                
+        > position : [int, int]
+            It contains an array with the 2D position of the LatexVertex
+            
+        > name  : ?
+        > color : ?
+            These two parameters are two generic filds for storing some needed information about the LatexVertex
+            es. if we have a vertex v with a particular color (red) and a particular hight value (3.7m) we can set
+                these two fields as the string 'red' and the double 3.7
+        
+        -----------
+    """
     def __init__(self,num,position,name,color):
         self.id = num
         self.connectedTo = {}
@@ -182,24 +250,34 @@ class LatexVertex:
     #     return self.id < o.id
     
     def addNeighbor(self,nbr,weight=0):
+        """ 
+        By using v.addNeighbor(w, 10) we add the edge from v to w with weight 10: 
+            If the edge already exist, we are updating its weight with 10. 
+            If the weight is not given, it is set as 0.
+        """
         self.connectedTo[nbr] = weight
         
     def setPosition(self, position):
+        """ This method update the vertex position """
         self.position = position
         
     def getPosition(self):
+        """ This method return the vertex position """
         return self.position
             
     def getConnections(self):
+        """ v.getConnections() returns the list of all the vertices connected to v """
         return self.connectedTo.keys()
         
     def getWeight(self,nbr):
+        """ It returns the weight of the edge from v to nbr """
         return self.connectedTo[nbr]
                 
     def __str__(self):
         return str(self.id) + ":position " + str(self.position) + "]\n"
     
     def getId(self):
+        """ It returns the vertex id """
         return self.id
     
 class LatexLattice:
