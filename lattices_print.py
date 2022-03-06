8ca0eb3268acb859292b1c5c0c3552c1b2f924af
Pico <lori.pichi@gmail.com>
2022-03-06 18:44:10 +0100

    version from 'reticoli'

--- lattices_print.py
+++ lattices_print.py
@@ -4,6 +4,44 @@ import math
 import ast
 import time
 
+def print_preambles():
+    print("""% ========== Tikz setting ==========
+%\\usepackage[svgnames]{xcolor}
+\\usepackage{tikz}
+\\usetikzlibrary{decorations.markings}
+\\usetikzlibrary{shapes.geometric}
+\\pagestyle{empty}
+
+\pgfdeclarelayer{edgelayer}
+\pgfdeclarelayer{nodelayer}
+\pgfsetlayers{edgelayer,nodelayer,main}
+
+\\tikzstyle{none}=[inner sep=0pt]
+
+\\tikzstyle{rn}=[circle,fill=red,draw=black,line width=0.8 pt]
+\\tikzstyle{gn}=[circle,fill=lime,draw=black,line width=0.8 pt]
+\\tikzstyle{yn}=[circle,fill=yellow,draw=black,line width=0.8 pt]
+\\tikzstyle{blstyle}=[circle,fill=black,draw=black]
+\\tikzstyle{wstyle}=[circle,fill=white,draw=black]
+\\tikzstyle{little}=[circle,fill=gray,draw=gray,scale=0.5 pt]
+
+\\tikzstyle{simple}=[-,draw=white,line width=3.000]
+\\tikzstyle{arrow}=[->,draw=darkgray,line width=2.000]
+\\tikzstyle{tick}=[-,draw=black,postaction={decorate},decoration={markings,mark=at position .5 with {\draw (0,-0.1) -- (0,0.1);}},line width=2.000]
+\\tikzstyle{redstyle}=[-,draw=red,line width=3.000]
+\\tikzstyle{bluestyle}=[-,draw=blue,line width=3.000]
+\\tikzstyle{greenstyle}=[-,draw=lime,line width=3.000]
+\\tikzstyle{flow}=[->,draw=green,line width=2.000]
+\\tikzstyle{redarrow}=[-latex,draw=red,line width=3.000]
+\\tikzstyle{greenarrow}=[-latex,draw=green,line width=3.000]
+\\tikzstyle{bluearrow}=[-latex,draw=blue,line width=3.000]
+\\tikzstyle{axe}=[->,draw=black,line width=2.000]
+\\tikzstyle{thiny}=[-,draw=lightgray,line width=1.000]
+\pgfplotsset{compat=1.17}
+
+\\usepackage{tikzscale}
+""")
+    
 class LatexGraph:
     def __init__(self, fp= None):
         self.vertices = {}
@@ -14,6 +52,7 @@ class LatexGraph:
         self.prefix = "%s"
         self.title = ""
         self.sufix = ""
+        self.nodeprefix = ""
 
     # ---------------- Graph function ---------------------        
     def addVertex(self,key,position,name=None,color=None):
@@ -96,18 +135,20 @@ class LatexGraph:
                     vertex_string = "\color{white} %s" % v.name
             
 
-            print("\t\t\\node [style=%s] (%d) at (%1.3f,%1.3f) {%s};" % (vertex_color, i, v.position[0], v.position[1], vertex_string), file= self.output)
+            print("\t\t\\node [style=%s] (%d%s) at (%1.3f,%1.3f) {%s};" % (vertex_color, i, self.nodeprefix, v.position[0], v.position[1], vertex_string), file= self.output)
             
         
     def edge_middle_string(self, v, u, s=None):
         middle_string = "to"
         return middle_string
         
-    def edges(self, other= None):
+    def edges(self, translated):
         for i in self.vertices:
             v = self.getVertex(i)
             for u in v.connectedTo:
-                print("\t\t\draw [style=%s] (%d) %s (%d);" % (self.edges_style, v.getId(), self.edge_middle_string(v, u), u.getId()), file= self.output)
+                print("\t\t\draw [style=%s] (%d%s) %s (%d%s);" % (self.edges_style, v.getId(), self.nodeprefix, self.edge_middle_string(v, u), u.getId(), self.nodeprefix), file= self.output)
+                if translated != None:
+                    print("\t\t\draw [style=thiny, color=green!75!white] (%1.3f,%1.3f) %s (%1.3f,%1.3f);" % ( v.position[0] - translated[0], v.position[1] - translated[1], self.edge_middle_string(v, u), u.position[0] - translated[0], u.position[1] - translated[1]), file= self.output)
                 
         
         
@@ -171,10 +212,15 @@ class LatexLattice:
         self.graph.addVertex(0, [0,0])
         self.minx = 1
         self.miny = 1
+        self.overset = 0.25
+        self.grid = True
         
         self.axes = LatexGraph()
         self.base = LatexGraph()
-        self.base_on = 0
+        self.base_on = False
+        self.parallelepid_on = False
+        self.corners = []
+        self.corners_radius = 0;
         
     # ---------- setting ----------
     def calc_min (self):
@@ -190,18 +236,18 @@ class LatexLattice:
         self.calc_min()
         
     def set_x(self, interval):
-        self.x = interval
+        self.x = [interval[0] - self.overset, interval[1] + self.overset]
         
     def set_y(self, interval):
-        self.y = interval
+        self.y = [interval[0] - self.overset, interval[1] + self.overset]
         
     def set_axes(self):
-        self.axes.addVertex(-1, [self.x[0], 0])
-        self.axes.addVertex(-2, [self.x[1], 0])
-        self.axes.addVertex(-3, [0, self.y[0]])
-        self.axes.addVertex(-4, [0, self.y[1]])
-        self.axes.addEdge(-1,-2)
-        self.axes.addEdge(-3,-4)
+        self.axes.addVertex(-100000, [self.x[0], 0])
+        self.axes.addVertex(-200000, [self.x[1], 0])
+        self.axes.addVertex(-300000, [0, self.y[0]])
+        self.axes.addVertex(-400000, [0, self.y[1]])
+        self.axes.addEdge(-100000,-200000)
+        self.axes.addEdge(-300000,-400000)
         self.axes.set_edges_style("axe")
         
     def set_base(self):
@@ -211,6 +257,18 @@ class LatexLattice:
         self.base.addEdge(0, 1)
         self.base.addEdge(0, 3)
         self.base.set_edges_style("bluearrow")
+        
+    def set_parallelepid(self):
+        self.parallelepid_on = True
+        
+    def add_corners_parallelepid(self, na, nb):
+        tx = na*(self.a[0]) + nb*(self.b[0])
+        ty = na*(self.a[1]) + nb*(self.b[1])
+        self.corners.append([tx,ty])
+        
+    def set_corners_radius(self, r):
+        self.corners_radius = r;
+        
         
     # ---------- generating ----------
     def is_visible (self, point):
@@ -243,30 +301,59 @@ class LatexLattice:
                         l.append(i)
                         i = i+1
                         
+                        
     # ---------- printing ----------
     
-    def printLatex(self):
+    def printLatex(self, nstyle = "little", estyle = "thiny", translated = False):
         self.set_axes()
         self.set_base()
-        self.graph.set_node_style("little")
-        self.graph.set_edges_style("thiny")
+        self.graph.set_node_style(nstyle)
+        self.graph.set_edges_style(estyle)
         
         
         print("\\begin{tikzpicture}", file= self.graph.output)
         
         print("\t\\begin{pgfonlayer}{nodelayer}", file= self.graph.output)
-        print("\t\\clip (%d,%d) rectangle (%d,%d);" % (self.x[0], self.y[0], self.x[1], self.y[1]), file= self.graph.output)
+        print("\t\\clip (%f,%f) rectangle (%f,%f);" % (self.x[0], self.y[0], self.x[1], self.y[1]), file= self.graph.output)
         
         self.axes.nodes()
         self.graph.nodes()
         print("\t\end{pgfonlayer}", file= self.graph.output)
         
         print("\t\\begin{pgfonlayer}{edgelayer}", file= self.graph.output)
-        print("\t\\clip (%d,%d) rectangle (%d,%d);" % (self.x[0], self.y[0], self.x[1], self.y[1]), file= self.graph.output)
-        self.axes.edges()
-        self.graph.edges()
+        print("\t\\clip (%f,%f) rectangle (%f,%f);" % (self.x[0], self.y[0], self.x[1], self.y[1]), file= self.graph.output)
+        if self.grid:
+            print("\t\\draw[thick,color=gray!25!white,step=1cm,dashed] (%f,%f) grid (%f,%f);" % (self.x[0], self.y[0], self.x[1], self.y[1]), file= self.graph.output)
+        self.axes.edges(None)
+        
+        if translated:
+            self.graph.edges([(self.a[0] + self.b[0])/2, (self.a[1] + self.b[1])/2])
+        else:
+            self.graph.edges(None)
+            
         if self.base_on:
             self.base.edges()
+        if self.parallelepid_on:
+            print("""
+            \\fill[lightgray] (0,0) -- (%d,%d) -- (%d,%d) -- (%d,%d) -- (0,0);
+            \\node[style=none] (p) at (%f,%f) {$\mathcal{P}(\mathcal{B})$};
+            """ % (self.a[0], self.a[1], self.a[0] + self.b[0], self.a[1] + self.b[1], self.b[0], self.b[1], (self.a[0] + self.b[0])/2, (self.a[1] + self.b[1])/2 ))
+            
+        for v in self.corners:
+            vx = v[0]
+            vy = v[1]
+            ax = self.a[0]
+            ay = self.a[1]
+            bx = self.b[0]
+            by = self.b[1]
+            r = self.corners_radius
+            print("\t\t\\begin{scope}", file= self.graph.output)
+            print("\t\t\t\clip (%f,%f) -- (%f,%f) -- (%f,%f) -- (%f,%f) -- (%f,%f);" % (vx,vy,vx+ax,vy+ay,vx+ax+bx,vy+ay+by,vx+bx,vy+by,vx,vy), file= self.graph.output)
+            print("\t\t\t\\fill[lime, opacity=0.5] (%f,%f) circle (%f);" % (vx,vy, r), file= self.graph.output)
+            print("\t\t\t\\fill[lime, opacity=0.5] (%f,%f) circle (%f);" % (vx+ax,vy+ay, r), file= self.graph.output)
+            print("\t\t\t\\fill[lime, opacity=0.5] (%f,%f) circle (%f);" % (vx+ax+bx,vy+ay+by, r), file= self.graph.output)
+            print("\t\t\t\\fill[lime, opacity=0.5] (%f,%f) circle (%f);" % (vx+bx,vy+by, r), file= self.graph.output)
+            print("\t\t\end{scope}", file= self.graph.output)
         print("\t\end{pgfonlayer}", file= self.graph.output)
         
         print("\end{tikzpicture}", file= self.graph.output)
